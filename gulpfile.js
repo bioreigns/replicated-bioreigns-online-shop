d2bb03713fe0ffa4a34bdf6fe93d2fda7b5a6aef
Ji20on <lmarkjason.20@gmail.com>
2020-05-23 06:08:48 +0800

    all pages done, some styles ongoing

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+
+
+//compile scss into css
+function style() {
+    //1. where is my scss file
+    return gulp.src('./scss/main.scss')
+        //2. pass that data file through sass compiler
+        .pipe(sass())
+        //where  do i save the compiled css
+        .pipe(gulp.dest('./dist/css'))
+}
+
+
+
+exports.style = style;
